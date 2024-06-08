d25f21de60ad2a6c82a4e139950aa51b12a70556
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-06-08 17:19:05 +0530

    change photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import myphoto from "./Assets/myphoto.png"
+import myphoto from "./Assets/myphoto.jpg"
 import mycv from './Assets/Mohd_Mustajab_Resume.pdf'
 import Typewriter from 'typewriter-effect';
 
